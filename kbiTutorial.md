b70c6560cc96eeaed1dfe5c58502956cad3e95ad
Shahjalal <muhommodshahjalal@gmail.com>
2024-12-19 12:34:52 +0600

    first commit

---
+++ kbiTutorial.md
@@ -0,0 +1,72 @@
+## keyboard remapping by keyd application
+
+```
+n = enter
+. = n
+capslock = esc,
+enter = .
+; = f3
+f3 = ;
+a hold f = ctrl+ f,
+; hold f = ctrl + f,
+s hold f = super + f,
+f hold f = alt + f,
+l hold f = shift + f ( that menas capital f),
+
+```
+
+## Arrow key
+
+```
+capslock + h = left arrow,
+caps + l = right arrow,
+caps + j = down arrow,
+caps + k = up arrow,
+
+```
+
+## Hyprland tutorial:
+
+```
+s+h = focus left,
+s+l = focus right,
+s+j = focus down,
+s+k = focus up,
+s+f = maximize/minimize
+
+```
+
+## volume control
+
+```
+s+f+u = increasing volume,
+s+f+i = decrease volume,
+```
+
+## swapping windows
+
+```
+s+f+h = swap with left,
+s+f+l = swap with right,
+s+f+j = swap with down,
+s+f+k = swap with up,
+```
+
+## workspace tutorial
+
+```
+s+1/s+u = go workspace 1,
+s+2/s+i = go workspace 2,
+s+3 = go workspace 3,
+s+4 = go workspace 4,
+s+5 = go workspace 5, (in this way you can use upto 9 workspace)
+```
+
+## moving windows
+
+```
+s+shift+h = move  left,
+s+shift+l = move  right,
+s+shift+j = move  down,
+s+shift+k = move  up,
+```
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,44 @@
+import config from '../../config';
+import { TStudent } from '../student/student.interface';
+import { Student } from '../student/student.model';
+import { AcademicSemester } from './../academicSemester/academicSemester.model';
+import { TUser } from './user.interface';
+import { User } from './user.model';
+import { generateStudentId } from './user.utils';
+
+const createStudentIntoDB = async (password: string, payload: TStudent) => {
+  // create a user object
+  const userData: Partial<TUser> = {};
+
+  //if password is not given , use deafult password
+  userData.password = password || (config.default_password as string);
+
+  //set student role
+  userData.role = 'student';
+
+  // find academic semester info
+  const admissionSemester = await AcademicSemester.findById(
+    payload.admissionSemester,
+  );
+
+  //set  generated id
+  userData.id = await generateStudentId(admissionSemester);
+//Argument of type '(Document<unknown, {}, TAcademicSemester> & TAcademicSemester & { _id: ObjectId; }) | null' is not assignable to parameter of type 'TAcademicSemester'.
+  // Type 'null' is not assignable to type 'TAcademicSemester'.
+  // create a user
+  const newUser = await User.create(userData);
+
+  //create a student
+  if (Object.keys(newUser).length) {
+    // set id , _id as user
+    payload.id = newUser.id;
+    payload.user = newUser._id; //reference _id
+
+    const newStudent = await Student.create(payload);
+    return newStudent;
+  }
+};
+
+export const UserServices = {
+  createStudentIntoDB,
+};
